5faefc22623fff508c87331c0a3e3f667bcefe38
SiMiezz <simone.giordano5@studenti.unina.it>
2021-12-21 17:21:47 +0100

    ok

--- Demo/src/loginFrame.java
+++ Demo/src/loginFrame.java
@@ -19,9 +19,6 @@ public class loginFrame extends JFrame {
 	private JTextField textUser;
 	private JTextField textPwd;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -35,9 +32,6 @@ public class loginFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public loginFrame() {
 		setTitle("LOGIN");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,37 +42,37 @@ public class loginFrame extends JFrame {
 		contentPane.setLayout(null);
 		
 		textUser = new JTextField();
-		textUser.setBounds(158, 89, 148, 26);
+		textUser.setBounds(158, 71, 148, 26);
 		contentPane.add(textUser);
 		textUser.setColumns(10);
 		
 		textPwd = new JTextField();
-		textPwd.setBounds(158, 148, 148, 26);
+		textPwd.setBounds(158, 120, 148, 26);
 		contentPane.add(textPwd);
 		textPwd.setColumns(10);
 		
 		JLabel lblUser = new JLabel("User");
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUser.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblUser.setBounds(54, 89, 73, 26);
+		lblUser.setBounds(54, 69, 73, 26);
 		contentPane.add(lblUser);
 		
 		JLabel lblPwd = new JLabel("Password");
 		lblPwd.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPwd.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblPwd.setBounds(54, 146, 73, 26);
+		lblPwd.setBounds(54, 116, 73, 26);
 		contentPane.add(lblPwd);
 		
 		JRadioButton btnOp = new JRadioButton("Operatore");
-		btnOp.setBounds(208, 218, 109, 23);
+		btnOp.setBounds(197, 196, 109, 23);
 		contentPane.add(btnOp);
 		
 		JRadioButton btnStud = new JRadioButton("Studente");
-		btnStud.setBounds(208, 192, 109, 23);
+		btnStud.setBounds(197, 170, 109, 23);
 		contentPane.add(btnStud);
 		
 		JButton btnGo = new JButton("Login");
-		btnGo.setBounds(335, 120, 89, 23);
+		btnGo.setBounds(335, 95, 89, 23);
 		contentPane.add(btnGo);
 	}
 }
