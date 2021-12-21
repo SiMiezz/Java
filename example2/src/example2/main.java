2eea7dfe9960c500e309f61990572732f16560ad
gospi4 <giuse.costabile@studenti.unina.it>
2021-12-21 13:24:14 +0100

    rf

--- example2/src/example2/main.java
+++ example2/src/example2/main.java
@@ -3,7 +3,6 @@ package example2;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("ciao");
 		System.out.println("kuki");
 	}
